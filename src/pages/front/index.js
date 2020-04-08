687b77089ed49af123d19511e441fe6397dbcad1
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-04-08 21:26:05 +0100

    feat<frontend>: User scroll down to see all other movies according to release date

--- src/pages/front/index.js
+++ src/pages/front/index.js
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import { FiSearch } from 'react-icons/fi';
 import axios from 'axios';
+import LazyLoad from 'react-lazyload';
 
 function Front() {
   const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key={KEY_HERE}&language=en-US&page=${page}`).then((result) => {
-      setMovies([...movies, ...result.data.results]);
-      setPage(result.data.page);
-      console.log(result);
-    })
+    fetchData()
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+  }, [isFetching]);
+
+  function fetchData() {
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=27e2920c99b7658f5d89f82bc529233f&language=en-US&page=${page + 1}`).then((result) => {
+      setMovies([...movies, ...result.data.results]);
+      setPage(page + 1);
+    });
+  }
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) {
+      return;
+    }
+    console.log('Fetch more list items!');
+    setIsFetching(!isFetching);
+  }
 
   return (
     <div className='front-container'>
+    {/* MENU */}
     <div className='menu'>
       <div className="user">
         <img className='user-avatar' src="https://i7.pngguru.com/preview/748/338/407/computer-icons-avatar-person-user-avatar.jpg" alt="user avatar"/>
@@ -26,7 +42,7 @@ function Front() {
       <div className='list'>
         <ul>
           <li>
-            <p>Trending Movies</p>
+            <p>Lastest Movies</p>
             <p>{movies.length}</p>
           </li>
           <li>
@@ -39,9 +55,12 @@ function Front() {
         </ul>
       </div>
     </div>
+    {/* FAKE DIV FOR MENU SPACE */}
+    <div />
+    {/* BODY */}
     <div className='body'>
       <div className="body-header">
-        <p className='header-title'>Trending Movies</p>
+        <p className='header-title'>Lastest Movies</p>
         <div className='search' id='btn-input'>
           <div id="search-icon">
             <FiSearch size={22} color='#FFF' />
@@ -52,15 +71,16 @@ function Front() {
       <div className='body-movies'>
         {
           movies.map((movie) => {
-            let poster = "https://image.tmdb.org/t/p/original/" + movie.poster_path;
             return (
-              <div className='movie-card'>
-                <img src={poster} alt=""/>
-                <h5>{movie.title}</h5>
-              </div>
+              <LazyLoad offset={150} once>
+                <div className='movie-card'>
+                  <img src={"https://image.tmdb.org/t/p/original/" + movie.poster_path} alt=""/>
+                  <h5>{movie.title}</h5>
+                </div>
+              </LazyLoad>
             );
           })
-        }
+        } 
       </div>
     </div>
     </div>
